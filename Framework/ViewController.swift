73c0e228f4bd7c52331cdefc88f470448f64ae60
iRakesh <rakeshkothari1984@gmail.com>
2015-10-04 23:36:45 +0530

    Initial Commit

---
+++ Framework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Framework
+//
+//  Created by Anil Kothari on 04/10/15.
+//  Copyright © 2015 Rakesh Kothari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
